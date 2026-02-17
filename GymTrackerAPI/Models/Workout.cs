c2e4749caf129f0dbea5c88ddcee63e3dca5403b
Alexander <alexander@steen-joergensen.dk>
2026-02-17 12:36:28 +0100

    Created Exercise and Workout models

---
+++ GymTrackerAPI/Models/Workout.cs
@@ -0,0 +1,10 @@
+﻿namespace GymTrackerAPI.Models
+{
+	public class Workout
+	{
+		public int Id { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime FinishedAt { get; set; }
+		public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+	}
+}
